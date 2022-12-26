dda2aad57d74ad575604e111042f0f2e1288ff02
Ameen NA <ameen.na@cloud4c.com>
2022-12-26 22:03:32 +0530

    First Code Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,122 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BrandNav from './components/BrandNav';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+
+  const [products, setProducts] = useState([]);
+  const [sorted, setSorted] = useState();
+  const [unsorted,setUnsorted] = useState();
+  const [searchWord, setWord] = useState("");
+  const [categories, setCategories] = useState([]);
+  const [choosen, setChoosenCategory] = useState("");
+  const [sort, setSort] = useState(false);
+
+  //Load API on Component Mound
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+    .then(res=>res.json())
+    .then(json=>{
+      setProducts(json)
+      setUnsorted(json)
+      var temp=[...json,]
+      setSorted(temp.sort((ob1,ob2) => (ob1.title>ob2.title? 1: -1)))
+    })
+    fetch('https://fakestoreapi.com/products/categories')
+            .then(res=>res.json())
+            .then(json=>setCategories(json))
+  }, [])
+
+  useEffect(() => {
+    if(sort)
+    {
+      setProducts(sorted)
+    }
+    else{
+      setProducts(unsorted)
+    }
+  }, [sort])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-light'>
+      <BrandNav />
+      <div className='container'>
+        <div className='row'>
+            <div className='col-lg-8 my-4'>
+              <div className='input-group bg-body rounded border'>
+                    <input className='form-control form-control-lg border-0' type="text" 
+                        onChange={(e) => setWord(e.target.value.toLowerCase())}/>
+                    <span class="input-group-text bg-body border-0" id="basic-addon2">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                      </svg>
+                    </span>
+                </div>
+            </div>
+            <div className='col my-4'>
+              <DropdownButton
+                      size="lg"
+                      variant="outline-secondary"
+                      title={choosen?choosen:"Filter"}
+                      id="input-group-dropdown-1"
+                    >
+                      <Dropdown.Item href="#"  onClick={() => setChoosenCategory("")}>All</Dropdown.Item>
+                    {
+                        categories?.map((category,index) => 
+                        <Dropdown.Item href="#" key={index}  onClick={() => setChoosenCategory(category)}>{category}</Dropdown.Item>
+                        )
+                      }
+                </DropdownButton>
+            </div>
+            <div className='col my-4'>
+              <div class="form-check form-switch fs-4">
+                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" checked={sort} onClick={() => setSort(!sort)}/>
+                <label class="form-check-label" for="flexSwitchCheckDefault">Sort</label>
+              </div>
+            </div>
+        </div>
+        <div className="row">
+          {
+            products?.map((product,index) => 
+            product?.title?.split(' ').slice(0, 4).join(' ').toLowerCase().includes(searchWord) && (product?.category === choosen || choosen == "")?
+            <div className='col d-flex justify-content-center'>
+              <div class="card my-2 p-5 border-light shadow" style={{width: "260px", borderRadius: 25+"px"}}>
+                <div className='d-flex justify-content-center' style={{width: "220px", height: "250px"}}>
+                <img src={product.image} width="100%" className="card-img-top" alt="Image loading..."/>
+                </div>
+                <div class="card-body bg-white">
+                  <h5 className="card-title m-auto">{product.title.split(' ').slice(0, 4).join(' ')}</h5>
+                  <h6 class="card-subtitle mb-2 text-muted my-2">{product.price}
+                    <div className='star-rating d-flex float-end'>
+                      <span class={Math.round(product.rating.rate)>0?"fa fa-star checked":"fa fa-star"}></span>
+                      <span class={Math.round(product.rating.rate)>1?"fa fa-star checked":"fa fa-star"}></span>
+                      <span class={Math.round(product.rating.rate)>2?"fa fa-star checked":"fa fa-star"}></span>
+                      <span class={Math.round(product.rating.rate)>3?"fa fa-star checked":"fa fa-star"}></span>
+                      <span class={Math.round(product.rating.rate)>4?"fa fa-star checked":"fa fa-star"}></span>
+                    </div>
+                  </h6>
+                  {/* <p class="card-text">{product.description}.</p> */}
+                  <a href="#" class="btn btn-primary d-flex justify-content-center mt-auto">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
+                      <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
+                      <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                    </svg>
+                    Add to cart
+                  </a>
+                </div>
+                {/* <div class="card-footer">
+                  2 days ago
+                </div> */}
+              </div>
+            </div>
+            :""
+            )
+          }
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/BrandNav.js
@@ -0,0 +1,39 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+// import logo from "../logo.svg";
+
+function BrandNav() {
+  return (
+    <>
+      <Navbar className='sticky-top py-3' bg="white" variant="white">
+        <Container>
+          <Navbar.Brand href="#home">
+            <b className='text-warning fs-2'>Shopy</b>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Nav className="ms-auto">
+            <Nav.Link href="#deets" className='text-dark'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+              </svg>
+            </Nav.Link>
+            <Nav.Link href="#deets" className='text-dark'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+                <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+              </svg>  Call Center
+            </Nav.Link>
+            <Nav.Link eventKey={2} href="#memes" className='text-dark'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-truck" viewBox="0 0 16 16">
+                <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5v-7zm1.294 7.456A1.999 1.999 0 0 1 4.732 11h5.536a2.01 2.01 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456zM12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12v4zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+              </svg>  Shipping & Returns
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default BrandNav;
